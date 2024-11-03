6d780a1195077102d17c0fe98abe7408d81cde55
Olena2583 <132217065+Olena2583@users.noreply.github.com>
2024-11-03 16:22:45 +0000

    Deploying to gh-pages from @ Olena2583/goit-js-hw-10@15ddd809f63f61c0ad950c4e675aa8b2596d74ac 🚀

---
+++ assets/1-timer-Dx0Hd_AE.js
@@ -0,0 +1,2 @@
+import{i as a}from"./vendor-BL2cRG7m.js";function t(o){const n=Math.floor(o/864e5),s=Math.floor(o%864e5/36e5),e=Math.floor(o%864e5%36e5/6e4),c=Math.floor(o%864e5%36e5%6e4/1e3);return{days:n,hours:s,minutes:e,seconds:c}}console.log(t(2e3));console.log(t(14e4));console.log(t(2414e4));a.show({title:"Hey",message:"What would you like to add?"});
+//# sourceMappingURL=1-timer-Dx0Hd_AE.js.map
