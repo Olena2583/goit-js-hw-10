959773142972b7860f5391f2c34aef2211250849
Olena2583 <e.sorochan@o2.pl>
2024-11-04 19:44:57 +0100

    fix timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,31 +1,43 @@
 `use strict`;
-
 import flatpickr from 'flatpickr';
-
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+const startBtn = document.querySelector('#start-btn');
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const selectedDate = selectedDates[0];
+    const now = new Date();
+    if (selectedDate <= now) {
+      startBtn.disabled = true;
+      iziToast.show({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        color: 'red',
+        position: 'topCenter',
+      });
+    } else {
+      startBtn.disabled = false;
+    }
   },
 };
+
+flatpickr('#datetime-picker', options);
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -35,11 +47,10 @@ console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-iziToast.show({
-  title: 'Hey',
-  message: 'What would you like to add?',
-});
-
-import iziToast from 'izitoast';
-
-import 'izitoast/dist/css/iziToast.min.css';
+async function loadHTML(selector, url) {
+  const response = await fetch(url);
+  if (response.ok) {
+    const text = await response.text();
+    document.querySelector(selector).innerHTML = text;
+  }
+}
