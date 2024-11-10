d5be32e9b8528e74266a473736a1d228d700f0dc
Olena2583 <e.sorochan@o2.pl>
2024-11-10 14:15:59 +0100

    fix timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -27,6 +27,50 @@ const options = {
   },
 };
 
+class Timer {
+  constructor({ onTick }) {
+    this.isActive = false;
+    this.onTick = onTick;
+    this.intervalId = null;
+    this.init();
+  }
+  init() {
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    const startTime = Date.now();
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const time = this.getTimeComponents(deltaTime);
+      this.onTick(time);
+    }, 1000);
+  }
+  getTimeComponents(time) {
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { hours, mins, secs };
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
 flatpickr('#datetime-picker', options);
 
 function convertMs(ms) {
